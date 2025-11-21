e79c3f064e364e6ac61dc1912df004e91170f3ed
Al Matty <al@almatty.com>
2025-11-21 15:40:38 +0100

    restored a prior version

--- transform/models/intermediate/int_companies.sql
+++ transform/models/intermediate/int_companies.sql
@@ -1,31 +1,33 @@
-with users as (
-    select * from {{ ref('int_users') }}
+with staging as (
+    select * from {{ ref('stg_amplitude_events') }}
 ),
 
 ip_addresses as (
     select * from {{ ref('int_ip_addresses') }}
 ),
 
+user_companies as (
+    select distinct
+        lower(split_part(user_id, '@', 2)) as company,
+        ip_address
+    from staging
+    where user_id is not null
+        and contains(user_id, '@')
+        and lower(split_part(user_id, '@', 2)) not in (
+            'gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com',
+            'aol.com', 'icloud.com', 'mail.com', 'protonmail.com',
+            'live.com', 'msn.com', 'ymail.com', 'googlemail.com',
+            'me.com', 'mac.com', 'comcast.net', 'verizon.net'
+        )
+),
+
 company_extraction as (
     select distinct
-        {{ dbt_utils.generate_surrogate_key(['company', 'i.ip_address']) }} as company_id,
-        company,
+        {{ dbt_utils.generate_surrogate_key(['c.company', 'i.ip_address']) }} as company_id,
+        c.company,
         i.ip_address
-    from (
-        select
-            lower(split_part(u.user_id, '@', 2)) as company,
-            u.ip_address_id
-        from users u
-        where u.user_id is not null
-            and contains(u.user_id, '@')
-            and lower(split_part(u.user_id, '@', 2)) not in (
-                'gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com',
-                'aol.com', 'icloud.com', 'mail.com', 'protonmail.com',
-                'live.com', 'msn.com', 'ymail.com', 'googlemail.com',
-                'me.com', 'mac.com', 'comcast.net', 'verizon.net'
-            )
-    ) companies
-    join ip_addresses i on companies.ip_address_id = i.ip_address_id
+    from user_companies c
+    join ip_addresses i on c.ip_address = i.ip_address
 )
 
 select * from company_extraction
